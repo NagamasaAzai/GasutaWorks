57b190e05abab062e610d5c00a461a443c34eca9
Gasuta <sonicx1203@gmail.com>
2024-04-16 12:52:26 +0700

    Добавили комментарии в исходный код

--- context1.c
+++ context1.c
@@ -1,3 +1,4 @@
+//Включение бибилиотек
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -5,8 +6,10 @@
 //функция программы
 int main(int argc, char *argv[], char *envp[]) {
 	int result;
+	//Запуск программы с аргументом otchet_16_04_2024.txt
 	result = execle("/bin/cat", "/bin/cat", "otchet_16_04_2024.txt", 0, envp);
+	//printf выполняется только в случае ошибки вызова
 	printf("Ошибка при выполнении системного вызова execle()\n");
 	exit(-1);
-	return 0;
+	return 0; //никогда не выполняется
 }
