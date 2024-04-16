eeac8ac16a65c5c704b22d0a3360289a71712285
Gasuta <sonicx1203@gmail.com>
2024-04-16 12:45:38 +0700

    Создали компелируемый исходный код

---
+++ context1.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+//функция программы
+int main(int argc, char *argv[], char *envp[]) {
+	int result;
+	result = execle("/bin/cat", "/bin/cat", "otchet_16_04_2024.txt", 0, envp);
+	printf("Ошибка при выполнении системного вызова execle()\n");
+	exit(-1);
+	return 0;
+}
